eb8c2f2282b284e6b514ce75274f7e2716927b85
aer2017 <835412083@qq.com>
2019-10-30 23:35:50 +0800

    2

---
+++ src/views/Mine/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="main">
+      <Header title="我的喵喵"/>
+      <Login />
+      <TagBar />
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Header'
+import TagBar from '@/components/TagBar'
+import Login from '@/components/Login'
+export default {
+    name:'Mine',
+    components:{
+        Header,
+        TagBar,
+        Login
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
